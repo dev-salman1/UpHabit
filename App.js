a42bee269fd4224ad5aeb3fe3c21e7af6036b832
dev-salman1 <salmankhalidg504@gmail.com>
2025-01-16 00:41:51 +0500

    Native Wind Setup

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import BootSplash from 'react-native-bootsplash';
+import './global.css';
 
 const App = () => {
   React.useEffect(() => {
     const init = async () => {};
-
     init().finally(async () => {
       await BootSplash.hide({fade: true});
     });
   }, []);
   return (
-    <View>
+    <View className="flex-1 items-center justify-center bg-red">
       <Text>App</Text>
     </View>
   );
